971f9581a67de0bc5d80b1a4cc4a85397b2d7b5c
Nam Tran <namnam13130606@gmail.com>
2024-05-26 17:05:12 +0700

    add Logout in settings menu

--- client/src/components/Chat/SettingsMenu.tsx
+++ client/src/components/Chat/SettingsMenu.tsx
@@ -1,6 +1,6 @@
 import { ListItem, ListItemButton, ListItemIcon, ListItemText, Popover, useTheme } from "@mui/material";
 import { SettingsMenuProps } from "../../utils/types";
-import { Contrast, DarkMode } from "@mui/icons-material";
+import { Contrast, DarkMode, Logout } from "@mui/icons-material";
 import { useThemeContext } from "../../contexts/ThemeContextProvider";
 
 
@@ -21,13 +21,31 @@ const SettingsMenu = ({
         anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
       >
         <ListItem>
-          <ListItemButton onClick={() => {
-            handleSetTheme(mode === 'light' ? 'dark' : 'light');
-          }}>
+          <ListItemButton
+            onClick={() => {
+              handleSetTheme(mode === "light" ? "dark" : "light");
+            }}
+          >
             <ListItemIcon>
-              {mode === 'light' ? <DarkMode /> : <Contrast />}
+              {mode === "light" ? <DarkMode /> : <Contrast />}
             </ListItemIcon>
-            <ListItemText primaryTypographyProps={{color: theme.palette.text.secondary}}>Switch to {mode === 'light' ? 'dark' : 'light'} mode</ListItemText>
+            <ListItemText
+              primaryTypographyProps={{ color: theme.palette.text.secondary }}
+            >
+              Switch to {mode === "light" ? "dark" : "light"} mode
+            </ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <Logout />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ color: theme.palette.text.secondary }}
+            >
+              Logout
+            </ListItemText>
           </ListItemButton>
         </ListItem>
       </Popover>
